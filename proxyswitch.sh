99f37ef652117fadb6293b7d67764791a0a1c8f4
Tan Yixiu <yixiu.tan@gmail.com>
2016-09-23 23:52:51 +0800

    commit script

---
+++ proxyswitch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MY_PROXY="http://test.pac"
+
+function proxyon(){
+  sudo networksetup -setautoproxystate "Wi-Fi" on
+  sudo networksetup -setautoproxyurl "Wi-Fi" $MY_PROXY
+}
+
+function proxyoff(){
+  sudo networksetup -setautoproxystate "Wi-Fi" off
+}
